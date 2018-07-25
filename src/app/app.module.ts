958b8655ee336022c1fb105b05619ea334fb6b45
Todd Halfpenny <todd@mobilecaddy.net>
2018-07-25 21:09:23 +0100

    Adding Ioinic native bits, needed for new MobileCaddy components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MobileCaddyModule } from 'mobilecaddy-angular';
-import { Network } from '@ionic-native/network';
 
 // MobileCaddy - DO NOT REMOVE
 import { SettingsPage, MCOutboxPage } from 'mobilecaddy-angular';
@@ -13,6 +12,9 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 // Native
+import { Device } from '@ionic-native/device';
+import { File } from '@ionic-native/file';
+import { Network } from '@ionic-native/network';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -36,6 +38,8 @@ import { APP_CONFIG, AppConfig } from './app.config';
     SplashScreen,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
     { provide: APP_CONFIG, useValue: AppConfig },
+    File,
+    Device,
     Network
   ]
 })
